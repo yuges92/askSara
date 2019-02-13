e61928fdde499cfa80d302a54c48970b9f75b3c7
Yugeswaran Sivanathan <sivayuges@gmail.com>
2019-02-13 15:28:48 +0000

    few change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,20 +40,20 @@ BANNER
 topic
 =============================================== -->
 <section id="as-group">
-  <div class="container">
+  <div class="col-10 p-5 mx-auto">
     <!-- Title -->
     <h2>Expert, personalised advice and information at your fingertips</h2>
     <div class="row">
       @foreach ($mainTopics as $mainTopic)
-      <div class="col-lg-3 group-box wow fadeInRight" data-wow-duration=".2s" data-wow-delay=".4s">
+      <div class="col-sm-6 col-lg-3 group-box wow fadeInRight w-100 mt-4" data-wow-duration=".2s" data-wow-delay=".4s">
         <div class="group-wrapper">
           <div class="group-box-img">
             <a href="/mainTopics/{{$mainTopic->mainTopic_id}}" class="inline-link">
               <img src="{{$mainTopic->filename}}" class="img-fluid" alt="Your health">
             </a>
           </div>
-          <div class="top-group">
-            <h5>{{$mainTopic->name}}</h5>
+          <div class="top-group p-1">
+            <h3>{{$mainTopic->name}}</h3>
             <p>{{$mainTopic->description}}</p>
             <a href="/mainTopics/{{$mainTopic->mainTopic_id}}" class="inline-link">Choose a topic</a>
           </div>
